3978690cbbc08ab85bfa71398721686e3da10df1
maayami <mayank16meena@gmail.com>
2021-08-15 23:59:06 +0530

    Add two more use-cases

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import data_extraction
 import response
 
+
 def chat_bot():
     # Load the config variables
     load_dotenv()
@@ -21,20 +22,32 @@ def chat_bot():
             parsed_message = json.loads(stream_messages.decode('utf-8'))
             message_sender = parsed_message["fromUser"]["username"]
             message = parsed_message["text"]
-            if message.startswith('@' + CHATBOT_NAME):
-                BOTANSWER = "**{}** I am currently learning this feature try after some time.".format(CHATBOT_NAME)
+
+            if message.startswith('@' + CHATBOT_NAME + " -help"):
+                BOTANSWER = "**{}** will be launched on 16th Aug.".format(CHATBOT_NAME)
                 gitter.messages.send(ROOM, BOTANSWER)
+
+            elif message.startswith('@' + CHATBOT_NAME + " -p"):
+                BOTANSWER = "**{}** will be launched on 16th Aug.".format(
+                    CHATBOT_NAME)
+                gitter.messages.send(ROOM, BOTANSWER)
+
             elif not message.startswith('**{}**'.format(CHATBOT_NAME)):
                 __data__ = data_extraction.process_message(message.lower())
+
                 if __data__ == [-1]:
                     pass
-                    # Do noting
+
                 elif __data__ == [0]:
-                    BOTANSWER = response.default_suggestion_answer(message_sender)
+                    BOTANSWER = response.default_suggestion_answer(
+                        message_sender)
                     gitter.messages.send(ROOM, BOTANSWER)
+
                 else:
-                    BOTANSWER = response.project_suggestion_answer(message_sender,__data__)
+                    BOTANSWER = response.project_suggestion_answer(
+                        message_sender, __data__)
                     gitter.messages.send(ROOM, BOTANSWER)
+
 
 if __name__ == "__main__":
     chat_bot()
