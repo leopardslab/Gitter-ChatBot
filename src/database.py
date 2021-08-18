4290cc0831f16f30f118a4eb54fee746a293d0c8
maayami <mayank16meena@gmail.com>
2021-08-19 00:55:32 +0530

    remove unnecessary comments from database.py

--- src/database.py
+++ src/database.py
@@ -18,30 +18,6 @@ client = pymongo.MongoClient(CONNECTION_STRING)
 db = client[DB_NAME]
 collection = db[COLLECTION_NAME]
 
-# Store some demonstration information
-# project1 = {
-#     "tags": ["python", "shell", "machine learning", "ml"],
-#     "name":
-#     "Gitter-ChatBot",
-#     "github-link":
-#     "https://github.com/leopardslab/Gitter-ChatBot",
-#     "gitter-link":
-#     "https://gitter.im/LeaopardLabs/Gitter-ChatBot",
-#     "good-first-issues":
-#     "https://github.com/leopardslab/Gitter-ChatBot/labels/good%20first%20issue"
-# }
-
-# collection.insert_one(project1)
-
-# Query the database for projects and interests, to find some projects
-# def find_projects(queries):
-#     for q in queries:
-#         projects = collection.find({"tags": query})
-#     return project
-# for x in all_projects:
-# print(x)
-
-
 # Use Github API to fetch all the community projects
 def fetch_projects():
     print("Fetching list of projects from the Github-API...")
